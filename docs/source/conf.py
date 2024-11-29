ae773f184dcc980bd240ab1934901ee34570774f
whitesejin <whitesejin4169@gmail.com>
2024-11-30 03:22:30 +0900

    modified rst5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))  # 현재 디렉토리를 경로에 추가
+
+
 project = 'Sej-dule'
 copyright = '2024, 24-2-Sej-dule'
 author = '24-2-Sej-dule'
