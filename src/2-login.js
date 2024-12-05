9262732b50c41aea7e3b5f3eb056bc952e2f1566
장서현 <jsh030213@gmail.com>
2024-12-06 01:41:47 +0900

    Modified 2-login html, css files

--- src/2-login.js
+++ src/2-login.js
@@ -1,28 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
+const loginButton = document.querySelector(".button-container__login");
+const text = document.querySelector("input");
+
+document.addEventListener("keyup", () => {
   const loginForm = document.getElementById("loginForm");
 
-  loginForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  if (text.value.trim() !== "") {
+    loginButton.disabled = false;
+    loginButton.classList.add("active");
+  } else {
+    loginButton.disabled = true;
+    loginButton.classList.remove("active");
+  }
+});
 
-    const formData = new FormData(loginForm);
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    try {
-      const response = await fetch("/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: formData.get("email"),
-          password: formData.get("password"),
-        }),
-      });
-      if (!response.ok) throw Error();
-      const token = await response.json();
-      console.log(token);
-      location.href = "/4-chat.html";
-    } catch (e) {
-      alert("로그인에 실패했습니다.");
-    }
-  });
+  const formData = new FormData(loginForm);
+
+  try {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+    });
+    if (!response.ok) throw Error();
+    const token = await response.json();
+    console.log(token);
+    location.href = "/4-chat.html";
+  } catch (e) {
+    alert("로그인에 실패했습니다.");
+  }
 });
